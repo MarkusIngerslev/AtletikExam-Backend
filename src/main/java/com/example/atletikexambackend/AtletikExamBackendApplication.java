9307ed9d80d212d88fe65a269d1c650ca3fa1aac
MarkusIngerslev <markusolsen2004@gmail.com>
2024-06-20 10:27:49 +0200

    Init backend

---
+++ src/main/java/com/example/atletikexambackend/AtletikExamBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.atletikexambackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtletikExamBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtletikExamBackendApplication.class, args);
+    }
+
+}
