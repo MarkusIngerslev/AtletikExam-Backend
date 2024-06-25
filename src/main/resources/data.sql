4f4ff6b14449a3b0f8eb59ed4fdbb2543c8a92bf
MarkusIngerslev <markusolsen2004@gmail.com>
2024-06-25 16:25:36 +0200

    Mindre ændring til sql data.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,17 +12,17 @@ INSERT INTO disciplin (navn, resultattype) VALUES ('400-meterløb', 'sekunder');
 INSERT INTO disciplin (navn, resultattype) VALUES ('Hammerkast', 'meter');
 
 -- //////////////////// Resultater //////////////////// --
-INSERT INTO resultat (resultattype, dato, resultatvalue, deltager_id, disciplin_id) VALUES ('Højdespring', '2024-06-20', 2.03, 1, 1);
+-- INSERT INTO resultat (resultattype, dato, resultatvalue, deltager_id, disciplin_id) VALUES ('Højdespring', '2024-06-20', 2.03, 1, 1);
 INSERT INTO resultat (resultattype, dato, resultatvalue, deltager_id, disciplin_id) VALUES ('Højdespring', '2021-06-20', 1.83, 2, 1);
 INSERT INTO resultat (resultattype, dato, resultatvalue, deltager_id, disciplin_id) VALUES ('Højdespring', '2021-06-20', 1.96, 3, 1);
 
 INSERT INTO resultat (resultattype, dato, resultatvalue, deltager_id, disciplin_id) VALUES ('Længdespring', '2021-06-20', 6.23, 1, 2);
-INSERT INTO resultat (resultattype, dato, resultatvalue, deltager_id, disciplin_id) VALUES ('Længdespring', '2021-06-20', 5.65, 2, 2);
+-- INSERT INTO resultat (resultattype, dato, resultatvalue, deltager_id, disciplin_id) VALUES ('Længdespring', '2021-06-20', 5.65, 2, 2);
 INSERT INTO resultat (resultattype, dato, resultatvalue, deltager_id, disciplin_id) VALUES ('Længdespring', '2021-06-20', 5.23, 3, 2);
 
 INSERT INTO resultat (resultattype, dato, resultatvalue, deltager_id, disciplin_id) VALUES ('400-meterløb', '2021-06-20', 61.54, 1, 3);
 INSERT INTO resultat (resultattype, dato, resultatvalue, deltager_id, disciplin_id) VALUES ('400-meterløb', '2021-06-20', 52.67, 2, 3);
-INSERT INTO resultat (resultattype, dato, resultatvalue, deltager_id, disciplin_id) VALUES ('400-meterløb', '2021-06-20', 55.23, 3, 3);
+-- INSERT INTO resultat (resultattype, dato, resultatvalue, deltager_id, disciplin_id) VALUES ('400-meterløb', '2021-06-20', 55.23, 3, 3);
 
 INSERT INTO resultat (resultattype, dato, resultatvalue, deltager_id, disciplin_id) VALUES ('Hammerkast', '2021-06-20', 23.23, 1, 4);
 INSERT INTO resultat (resultattype, dato, resultatvalue, deltager_id, disciplin_id) VALUES ('Hammerkast', '2021-06-20', 23.23, 2, 4);
